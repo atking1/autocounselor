d8fbca43402843acff3d7c9a9ae1366b9e846441
Alexander King <atking1@go.olemiss.edu>
2020-05-01 13:39:43 -0500

    expand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,30 +11,29 @@ import 'firebase/database';
 export class AppComponent {
   title = 'Autocounselor';
   counter = 0;
-  stronglyDisagree: boolean;
-  disagree: boolean;
-  neutral: boolean;
-  agree: boolean;
-  stronglyAgree: boolean;
+  hidden = true;
+  weights: [];
   questions: any[];
-  collectedAnswers = [];
+  majorGroups: any[];
+  largestIndex: number;
   scores: Array<number> = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  options: string[] = ['Strongly Disagree', 'Disagree', 'Neutral', 'Agree', 'Strongly Agree'];
   constructor(db: AngularFireDatabase) {
     db.list('/questions').valueChanges().subscribe(questions => {
       this.questions = questions;
       console.log(this.questions);
-      this.collectedAnswers.fill(this.questions.length);
-      console.log(this.collectedAnswers);
-      // while (this.collectedAnswers.length !== this.questions.length) {
-//
-     // }
+    });
+    db.list('/majorGroups').valueChanges().subscribe(majorGroups => {
+      this.majorGroups = majorGroups;
+      console.log(this.majorGroups);
     });
   }
   decrementCounter() {
     if (this.counter >= 5) {
       this.counter -= 5;
     } else {
+      (document.getElementById('PrevButton') as HTMLInputElement).disabled = true;
+    }
+    if (this.counter === 0) {
       (document.getElementById('PrevButton') as HTMLInputElement).disabled = true;
     }
     (document.getElementById('NextButton') as HTMLInputElement).disabled = false;
@@ -47,27 +46,57 @@ export class AppComponent {
       (document.getElementById('NextButton') as HTMLInputElement).disabled = true;
       (document.getElementById('FinishButton') as HTMLInputElement).disabled = false;
     }
+    if (this.counter >= this.questions.length || this.counter === this.questions.length) {
+      (document.getElementById('NextButton') as HTMLInputElement).disabled = true;
+      (document.getElementById('FinishButton') as HTMLInputElement).disabled = false;
+    }
     (document.getElementById('PrevButton') as HTMLInputElement).disabled = false;
     console.log(this.counter);
   }
   computeScores() {
+    for (const question of this.questions) {
+      if (question.chosenOption === 0) {
+        console.log('Questions incomplete');
+        return;
+      }
+    }
     document.getElementById('questionList').style.visibility = 'hidden';
     document.getElementById('PrevButton').style.visibility = 'hidden';
     document.getElementById('NextButton').style.visibility = 'hidden';
     document.getElementById('FinishButton').style.visibility = 'hidden';
+    let i: number;
+    console.log(this.questions.length);
     for (const question of this.questions) {
+      this.weights = question.weight.split(',');
+      console.log(this.weights);
       console.log(question.chosenOption);
-      if (question.chosenOption === 1) {
-      } else if (question.chosenOption === 2) {
-
-      } else if (question.chosenOption === 3) {
-      } else if (question.chosenOption === 4) {
-      } else {
-        break;
+      for (i = 0; i <= this.weights.length; i++) {
+        if (this.weights[i] !== 0) {
+          if (question.chosenOption === 1) {
+            this.scores[i] -= (2 * this.weights[i]);
+          } else if (question.chosenOption === 2) {
+            this.scores[i] -= this.weights[i];
+          } else if (question.chosenOption === 4) {
+            this.scores[i] += this.weights[i];
+          } else if (question.chosenOption === 5) {
+            this.scores[i] += (2 * this.weights[i]);
+          } else {
+            continue;
+          }
+        }
       }
     }
-  }
-  onChanges(val) {
-    console.log(val);
+    let largestScore = 0;
+    this.largestIndex = 0;
+    let largest = this.majorGroups[0];
+    for (i = 0; i <= this.scores.length; i++) {
+      if (this.scores[i] > largestScore) {
+        largestScore = this.scores[i];
+        this.largestIndex = i;
+        largest = this.majorGroups[i];
+      }
+    }
+    console.log(this.scores);
+    this.hidden = false;
   }
 }
